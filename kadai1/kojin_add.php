3a72cccbcd1e4b834eafb7dad8d7e39c193ab8f9
komei6351 <220015@jc-21.jp>
2022-12-13 14:52:36 +0900

    12134

--- kadai1/kojin_add.php
+++ kadai1/kojin_add.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>個人情報</title>
-        <link type="text/css" href="kojin_list.css" rel="stylesheet">
+        <link type="text/css" href="kojin_add.css" rel="stylesheet">
         <meta http-equiv="Cache-Control" content="no-cache">
 
     </head>
@@ -17,13 +17,13 @@
         ふりがなを入力してください。<br />
         <input type="text" name="huri" style="width:300px"><br />
         郵便番号を入力してください。<br />
-        <input type="text" name="yubin" style="width:300px"><br />
+        <input type="number" name="yubin" style="width:300px"><br />
         住所を入力してください。<br />
         <input type="text" name="jusyo" style="width:300px"><br />
         電話番号を入力してください。<br />
-        <input type="text" name="den" style="width:300px"><br />
+        <input type="tel" name="den" style="width:300px"><br />
         E-mailアドレスを入力してください。<br />
-        <input type="text" name="mail" style="width:300px"><br />
+        <input type="email" name="mail" style="width:300px"><br />
         <br />
         <button type="button" onclick="history.back()">戻る</button>
         <button type="submit">ＯＫ</button>
